05baf10a35bf9046e8bca3e4af624f6cf362e3ce
tasbeeh43 <tasbeehmamduh2004@gmail.com>
2024-09-16 15:14:40 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+// document.getElementById("example").innerHTML = "hello js";
+
+
+// document.getElementById("example").style.color = "rgb(255,0,0)";
+
+
+// document.getElementById("example").style.textAlign = "center";
+
+
+// //  alert("tasbeeh ");
+
+
+// // console.log("sdsjd kbr"); 
+
+
+// // document.write("3333");
+
+
